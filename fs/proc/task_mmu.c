75aa2675988eea89c84f14c2f0b34ddce87d7494
Massatriof16 <massatrioo09@gmail.com>
2025-02-16 06:34:54 +0700

    Update task_mmu.c

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -25,7 +25,9 @@
 #include <asm/tlb.h>
 #include <asm/tlbflush.h>
 #include "internal.h"
-
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
 #define SEQ_PUT_DEC(str, val) \
 		seq_put_decimal_ull_width(m, str, (val) << (PAGE_SHIFT-10), 8)
 void task_mem(struct seq_file *m, struct mm_struct *mm)
