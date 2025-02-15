e13be47d1eafddd260f4c5525c1571dc15b10fc5
Massatriof16 <massatrioo09@gmail.com>
2025-02-16 05:25:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,40 @@
-#!/bin/bash
-sudo apt update -y && sudo apt upgrade -y && sudo apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && sudo apt install build-essential -y && sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && sudo apt install pigz -y && sudo apt install python2 -y && sudo apt install python3 -y && sudo apt install cpio -y && sudo apt install lld -y && sudo apt install llvm -y
+echo "Updating package lists..."
+sudo apt update -y && sudo apt upgrade -y
+
+# Daftar paket yang dibutuhkan
+PACKAGES=(
+  nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl
+  python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi
+  software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu
+  libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev
+  libsqlite3-dev pigz python2 python3 cpio lld llvm
+)
+
+# Cek dan install paket jika belum terinstal
+for pkg in "${PACKAGES[@]}"; do
+  if ! dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "installed"; then
+    echo "Installing $pkg..."
+    sudo apt install -y "$pkg"
+  else
+    echo "$pkg is already installed, skipping..."
+  fi
+done
+
+echo "All necessary packages are installed!"
+
+
+
+read -p "Apakah ingin menggunakan KSU? : " confirm
+if [ "${confirm}" = y ] || [ "${confirm}" = Y ]; then
+read -p "Version : " versi
+if [ -z "${versi}" ]; then
+echo versi tidak boleh kosong! Diaggap tidak menggunakan ksu
+else
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v${versi}
+fi
+else
+echo tidak menggunakan Ksu
+fi
 
 
 git clone https://github.com/kdrag0n/aarch64-elf-gcc.git --depth=1
@@ -16,7 +51,7 @@ PATH="/usr/bin:$PATH"
 
 # build kernel
 
-make -j$(nproc --all) ARCH=arm64 O=out \
+make --j$(nproc --all) ARCH=arm64 O=out \
                       HOSTCC="clang" \
                       HOSTCXX="clang++" \
                       CC="clang" \
@@ -35,4 +70,3 @@ make -j$(nproc --all) ARCH=arm64 O=out \
                       
                       
 curl -F document=@"out/arch/arm64/boot/Image.gz" https://api.telegram.org/bot7158353974:AAGsmJmfMHXIK9Pj2GIdo6u1eTH2HTR_HHQ/sendDocument?chat_id=6561499315
-
